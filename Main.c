c0842867a00ee8d546a8a7667f4eced9c94e429f
Voxel <tux@xyecoc.com>
2026-03-27 20:20:25 +1000

    added

---
+++ Main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+bool iswin = true;
+int Balance = 150;
+
+int main() {
+
+	setlocale(LC_ALL, "ru_RU.UTF-8");
+
+	while (iswin) {
+		system("cls");
+		int bablo;
+		int user_input;
+		srand(time(NULL));
+
+		int dadep = 1 + rand() % 10;
+
+		if (Balance <= 0) {
+			fprintf(stdout, "Ты обанкротился...");
+
+			return;
+		}
+
+		fprintf(stdout, "твой баланс киви кошелька йоу: \033[36m%d\033[0m\n\n", Balance);
+
+		fprintf(stdout, "Васап, мабой у тебя отжали гуфи квартирку теперь тебе нужно отыграться и залутать 5000\n");
+
+		fprintf(stdout, "скока ты поставишь своих кровных? : ");
+		scanf_s("%d", &bablo);
+
+		if (bablo > Balance) {
+			fprintf(stdout, "на вашем киви кошельке не достачечно мани мани бро\n");
+			continue;
+		}
+
+		fprintf(stdout, "ай ай числищко то выпало угадывай иначе твои мани мани пропадут число от 1 до 10 действуй\n");
+
+		scanf_s("%d", &user_input);
+
+		
+
+		if (user_input == dadep) {
+			fprintf(stdout, "емае мой велосипед ты угадал да ну нафег");
+			Sleep(5000);
+			Balance *= bablo;
+		}
+		else {
+
+			fprintf(stdout, "ХАХАХАХАХАХАХАХАХА ТЫ ПРОИГРАЛ");
+			Sleep(5000);
+			Balance -= bablo;
+		}
+
+		if (Balance >= 5000) {
+			printf("о черт ты выиграл!");
+
+			Sleep(5000);
+			iswin = false;
+			break;
+		}
+	}
+
+	return 0;
+}
