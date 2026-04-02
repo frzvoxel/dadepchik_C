5c5b718df3abd2092587da8ace5e7accf18a6cdd
Voxel <tux@xyecoc.com>
2026-04-02 15:57:18 +1000

    Update Main.c

--- Main.c
+++ Main.c
@@ -7,18 +7,19 @@
 
 bool iswin = true;
 int Balance = 150;
+int otkyp = 5000;
 
 int main() {
-
+	srand(time(NULL));
 	setlocale(LC_ALL, "ru_RU.UTF-8");
 
 	while (iswin) {
 		system("cls");
 		int bablo;
 		int user_input;
-		srand(time(NULL));
 
 		int dadep = 1 + rand() % 10;
+		short minimal = 1;
 
 		if (Balance <= 0) {
 			Beep(500, 200);
@@ -29,7 +30,7 @@ int main() {
 
 		fprintf(stdout, "твой баланс киви кошелька йоу: \033[36m%d\033[0m\n\n", Balance);
 
-		fprintf(stdout, "Васап, мабой у тебя отжали гуфи квартирку теперь тебе нужно отыграться и залутать 5000\n");
+		fprintf(stdout, "Васап, мабой у тебя отжали гуфи квартирку теперь тебе нужно отыграться и залутать 5000 НО С КАЖДЫМ ПРОИГРЫШОМ ОНО УВЕЛИЧИВАЕТСЯ НА 1000\n");
 
 		fprintf(stdout, "скока ты поставишь своих кровных? : ");
 		scanf_s("%d", &bablo);
@@ -43,21 +44,22 @@ int main() {
 
 		scanf_s("%d", &user_input);
 
-		
 
-		if (user_input == dadep) {
+
+		if (abs(user_input - dadep) <= minimal) {
 			fprintf(stdout, "емае мой велосипед ты угадал да ну нафег");
 			Sleep(5000);
-			Balance *= bablo;
+			Balance += (bablo * 2);
 		}
 		else {
 
 			fprintf(stdout, "ХАХАХАХАХАХАХАХАХА ТЫ ПРОИГРАЛ");
 			Sleep(5000);
-			Balance -= bablo;
+			Balance /= (bablo / 2.0);
+			otkyp += 1000;
 		}
 
-		if (Balance >= 5000) {
+		if (Balance >= otkyp) {
 			printf("о черт ты выиграл!");
 			Beep(500, 200);
 
